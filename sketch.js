9e5e166d2623127f573efb685ad372031ca40e79
MrMatKubic <matkubik83@gmail.com>
2017-12-19 23:37:12 +0100

    Added Commentaries

--- sketch.js
+++ sketch.js
@@ -14,20 +14,23 @@ function setup()
 
 function draw()
 {
+    // CANVAS SETTINGS
     background(0);
     translate(width / 2, height / 2);
     noFill();
     stroke(255);
     strokeWeight(3);
 
+    // LOOP TO CALCULATE AND STORE POINTS
     for(let theta = 0; theta < TWO_PI * 30; theta += TWO_PI / 200)
     {
-        r = (Math.sqrt(cos(2 * theta))) * 120;
+        r = (2 * cos(6 * theta)) * 120; // THE R VALUE TO SET WITH A POLAR EQUATION
         x = r * cos(theta);
         y = r * sin(theta);
         points.push(createVector(x, y, theta));    
     }  
 
+    // LOOP TO DRAW POINTS WITH VERTICES
     beginShape();
     for(let i = 0; i < points.length; i++)
     {
