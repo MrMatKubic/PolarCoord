a78310da08d650d733f273506c0310610c7de204
MrMatKubic <matkubik83@gmail.com>
2017-12-20 16:52:14 +0100

    Finished Buttons Implementation

--- sketch.js
+++ sketch.js
@@ -2,6 +2,11 @@ let x, y;
 let r;
 let points = [];
 let theta = 0;
+var listEquations = document.getElementById("selEquation");
+var listEquation = listEquations.options[listEquations.selectedIndex].value;
+let equation = 0;
+var button = document.getElementById("startButton");
+let isStartButtonClicked;
 
 function setup()
 {
@@ -10,19 +15,26 @@ function setup()
     x = 0;
     y = 0;
     r = 250;
+    isStartButtonClicked = false;
 }
 
-function draw()
-{
-    // CANVAS SETTINGS
-    background(255);
-    translate(width / 2, height / 2);
-    noFill();
-    stroke(0);
-    strokeWeight(4);
+function clickOnDrawButton() 
+{ 
+    points.length = 0;
+    theta = 0; x = 0; y = 0;
+    isStartButtonClicked = !isStartButtonClicked;
+}
 
+function drawShape()
+{
     // LOOP TO CALCULATE AND STORE POINTS
-    r = (2 * cos(6 * theta)) * 140; // THE R VALUE TO SET WITH A POLAR EQUATION
+    listEquation = listEquations.options[listEquations.selectedIndex].value;    
+    if(this.listEquation === "2 * cos(6 * theta)") r = (2 * cos(6 * theta)) * 140;
+    else if(this.listEquation === "2 * cos(2 * theta)") r = (2 * cos(2 * theta)) * 140;
+    else if(this.listEquation === "5 * theta") r = (5 * theta);
+    else r = 150;
+
+    // THE R VALUE TO SET WITH A POLAR EQUATION
     x = r * cos(theta);
     y = r * sin(theta);
     points.push(createVector(x, y, theta));     
@@ -35,5 +47,16 @@ function draw()
     } 
     endShape();
 
-    if(theta < TWO_PI) theta += TWO_PI / 400;
+    if(theta < TWO_PI) theta += TWO_PI / 400;  
+}
+
+function draw()
+{
+    // CANVAS SETTINGS
+    background(255);
+    translate(width / 2, height / 2);
+    noFill();
+    stroke(0);
+    strokeWeight(4);
+    if(isStartButtonClicked) this.drawShape();
 }
